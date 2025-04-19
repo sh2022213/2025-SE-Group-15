1f337975208caa5cc88c22f34ac6f233b9e0068d
liuyutong-star <liuyutong2022213076@gmail.com>
2025-04-19 21:57:24 +0800

    controller

---
+++ FileParser.java
@@ -0,0 +1,13 @@
+package com.personalfinance.controller;
+
+import com.personalfinance.model.Transaction;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+// 定义文件解析器接口
+public interface FileParser {
+    // 解析文件的方法
+    List<Transaction> parse(File file) throws IOException;
+}
