7a678ed84344bfd047d972f7a01155f7b148a41a
liuyutong-star <liuyutong2022213076@gmail.com>
2025-04-19 23:03:43 +0800

    XlsFileParser_YutongLiu_updated
    XlsFileParser_YutongLiu_updated

--- src/controller/XlsFileParser.java
+++ src/controller/XlsFileParser.java
@@ -5,13 +5,13 @@ import com.personalfinance.model.Transaction;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.List;    
 
 /**
- * 预留 xls 文件解析器接口
+ * 预留 xls 文件解析器接口   
  */
 public class XlsFileParser implements FileParser {
-    @Override
+    @Override    
     public List<Transaction> parse(File file) throws IOException {
         // 这里是解析 xls 文件的具体实现
         // 目前只是占位，需要根据实际情况编写逻辑
