71243dde77b9001f184a5dd974fe0abf34f3b327
puppyGuo <134310256+HaitongLan@users.noreply.github.com>
2025-04-20 14:11:50 +0800

    Component_HaitongLan_updated
    Component_HaitongLan_updated

--- component/CurrencyTextField.java
+++ component/CurrencyTextField.java
@@ -1,5 +1,6 @@
+//包名
 package com.personalfinance.view.component;
-
+//引入
 import javax.swing.*;
 import javax.swing.text.NumberFormatter;
 import java.math.BigDecimal;
@@ -9,7 +10,7 @@ import java.text.ParseException;
 public class CurrencyTextField extends JFormattedTextField {
     private final NumberFormat format;
 
-    public CurrencyTextField() {
+    public CurrencyTextField() {//文本域
         format = NumberFormat.getNumberInstance();
         format.setMinimumFractionDigits(2);
         format.setMaximumFractionDigits(2);
@@ -24,7 +25,7 @@ public class CurrencyTextField extends JFormattedTextField {
         setValue(0.00);
     }
 
-    public BigDecimal getAmount() throws ParseException {
+    public BigDecimal getAmount() throws ParseException {//get账户
         Object value = getValue();
         if (value instanceof Number) {
             return BigDecimal.valueOf(((Number) value).doubleValue());
@@ -32,12 +33,12 @@ public class CurrencyTextField extends JFormattedTextField {
         return BigDecimal.ZERO;
     }
 
-    public void setAmount(BigDecimal amount) {
+    public void setAmount(BigDecimal amount) {//设置账户
         setValue(amount.doubleValue());
     }
 
     @Override
-    public void setValue(Object value) {
+    public void setValue(Object value) {//设置数值
         if (value instanceof BigDecimal) {
             super.setValue(((BigDecimal) value).doubleValue());
         } else {
