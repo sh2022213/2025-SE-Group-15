1bc982e8ee6c1230a7237b912a0e8a47dd38bed0
heyu-233 <211019825@qq.com>
2025-05-23 23:14:48 +0800

    Update MainSystem.java

--- src/main/java/com/personalfinance/MainSystem.java
+++ src/main/java/com/personalfinance/MainSystem.java
@@ -1,10 +1,13 @@
 package com.personalfinance;
 
+
 import com.personalfinance.controller.FinanceController;
 import com.personalfinance.view.LoginDialog;
 import com.personalfinance.view.MainFrame;
 
 import javax.swing.*;
+
+
 
 public class MainSystem {
     public static void main(String[] args) {
